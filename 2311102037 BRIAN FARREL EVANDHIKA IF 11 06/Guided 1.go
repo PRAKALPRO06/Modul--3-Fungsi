cdc03f3a4298fb51aa7ed8bda76f6d0e8b06efea
Brian Farrel Evandhika <128310486+B1216N@users.noreply.github.com>
2024-10-13 12:20:59 +0700

    Modul 3

---
+++ 2311102037 BRIAN FARREL EVANDHIKA IF 11 06/Guided 1.go
@@ -0,0 +1,27 @@
+//2311102037 BRIAN FARREL EVANDHIKA IF 11 06
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Scan(&a, &b)
+	if a >= b {
+		fmt.Println(permutasi(a, b))
+	} else {
+		fmt.Println(permutasi(b, a))
+	}
+}
+
+func faktorial(n int) int {
+	var hasil int = 1
+	var i int
+	for i = 1; i <= n; i++ {
+		hasil = hasil * i
+	}
+	return hasil
+}
+
+func permutasi(n, r int) int {
+	return faktorial(n) / faktorial(n-r)
+}
---
+++ 2311102037 BRIAN FARREL EVANDHIKA IF 11 06/Unguided 2.go
@@ -0,0 +1,64 @@
+//2311102037 BRIAN FARREL EVANDHIKA IF 11 06
+package main
+
+import (
+    "fmt"
+)
+
+// Fungsi f(x), g(x), dan h(x)
+func f(x int) int {
+    return x * x
+}
+
+func g(x int) int {
+    return x - 2
+}
+
+func h(x int) int {
+    return x + 1
+}
+
+// Komposisi fungsi: f(g(h(x)))
+func fogoh(x int) int {
+    return f(g(h(x)))
+}
+
+// Komposisi fungsi: g(h(f(x)))
+func gohof(x int) int {
+    return g(h(f(x)))
+}
+
+// Komposisi fungsi: h(f(g(x)))
+func hofog(x int) int {
+    return h(f(g(x)))
+}
+
+// Fungsi untuk menghitung faktorial
+func factorial(n int) int {
+    if n == 0 {
+        return 1
+    }
+    result := 1
+    for i := 2; i <= n; i++ {
+        result *= i
+    }
+    return result
+}
+
+func main() {
+    var a, b, c int
+    // Membaca input
+    fmt.Print("Masukkan a, b, c: ")
+    fmt.Scanf("%d %d %d", &a, &b, &c)
+
+    // Menghitung hasil
+    fogohResult := fogoh(a)
+    gohofResult := gohof(b)
+    hofogResult := hofog(c)
+
+    // Mencetak hasil sesuai dengan format contoh
+    fmt.Printf("%d\n", fogohResult)
+    fmt.Printf("%d\n", gohofResult)
+    fmt.Printf("%d\n\n", hofogResult)
+
+}
