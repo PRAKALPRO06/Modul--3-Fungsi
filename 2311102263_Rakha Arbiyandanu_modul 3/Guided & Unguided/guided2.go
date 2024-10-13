a6f8e44d7228fa41a2bfd9e290fd1f07866ec422
rakhaaja <89782159+rakhaaja@users.noreply.github.com>
2024-10-13 21:21:21 +0700

    Add files via upload

---
+++ 2311102263_Rakha Arbiyandanu_modul 3/Guided & Unguided/guided2.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func cekLuas(panjang, lebar, tinggi float64) float64 {
+	return 2 * (panjang*lebar + panjang*tinggi + lebar*tinggi)
+}
+
+func volume(panjang, lebar, tinggi float64) float64 {
+	return panjang * lebar * tinggi
+}
+
+func main() {
+	var panjang, lebar, tinggi float64
+
+	fmt.Print("Masukkan panjang permukaan: ")
+	fmt.Scanln(&panjang)
+	fmt.Print("Masukkan lebar permukaan: ")
+	fmt.Scanln(&lebar)
+	fmt.Print("Masukkan tinggi permukaan: ")
+	fmt.Scanln(&tinggi)
+
+
+	fmt.Println("Luas permukaan balok: ", cekLuas(panjang, lebar, tinggi))
+	fmt.Println("Volume balok: ", volume(panjang, lebar, tinggi))
+}
